9b0800b48321c89d8f73a90d77111996933e315e
0torrino1! <nestor.informatica95@gmail.com>
2021-02-13 19:40:26 +0000

    Help added

--- frontend/app/src/main/java/com/example/frontend/controller/MainActivity.kt
+++ frontend/app/src/main/java/com/example/frontend/controller/MainActivity.kt
@@ -1,16 +1,17 @@
-package com.example.frontend
+package com.example.frontend.controller
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import com.example.frontend.controller.WebView
+import com.example.frontend.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        listeners()
     }
 
     private fun listeners() {
