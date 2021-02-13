9a3963dcde7df24a395c8c990d693f7b43e98169
JavierMedina11 <javierinformatica1311@gmail.com>
2021-02-13 19:55:54 +0100

    AddFolder&HomeLogin_Activity

---
+++ frontend/app/src/main/java/com/example/frontend/controller/ui/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.example.frontend.controller.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.frontend.R
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        buttonSingIn.setOnClickListener {
+            goToMainActivity()
+        }
+    }
+    private fun goToMainActivity(){
+        val intent= Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ frontend/app/src/main/java/com/example/frontend/controller/ui/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.frontend.controller.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.example.frontend.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        listeners()
+    }
+
+    private fun listeners() {
+        val helpBtn = findViewById<Button>(R.id.helpBtn)
+        helpBtn.setOnClickListener {
+            Log.v("help","picao")
+            val intent = Intent(this, WebView::class.java)
+            startActivity(intent)
+        }
+    }
+}
